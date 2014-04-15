14fc2c5d9e14f993e876aa24fa1677f98198dd49
Clark Perkins <r.clark.perkins@gmail.com>
2014-04-15 12:44:42 -0500

    Added admin page back

--- SCAM/settings.py
+++ SCAM/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,3 +81,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
