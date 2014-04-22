7310149a9b9cc1abd8769d0b7f05ed48ae9d0816
Clark Perkins <r.clark.perkins@gmail.com>
2014-04-22 12:16:24 -0500

    Added calendar modal

--- Calendar/views.py
+++ Calendar/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
+from datetime import date
 #import calendarAPI as api
 
 # Create your views here.
@@ -8,10 +9,41 @@ from django.views.decorators.http import require_http_methods
 @require_http_methods(["GET"])
 def home(request):
     if request.session.has_key("username"):
+        events = []
+        
+        for i in range(1,51):
+            n = i % 4 * 15
+            if n is 0:
+                thestr = "00"
+            else:
+                thestr = str(n)
+            
+            n2 = i%37%12
+            
+            if n2 is 0:
+                n2+=12
+                    
+                    
+            n3 = i%12
+            
+            if n3 is 0:
+                n3 += 12
+            
+            n4 = i%28
+            
+            if n4 is 0:
+                n4 += 28
+            
+            events.append({
+                "name" : "My Event #"+str(i),
+                "date" : date(2014, n3, n4),
+                "time" : str(n2)+":"+thestr+" pm"
+            })
+
         context = {
             "username" : request.session["username"],
             "calendars" : ["Leisure", "Work", "School", "General", "Vanderbilt"],
-            "events" : ["My Event #1", "My Event #2", "My Event #3", "My Event #4", "My Event #1", "My Event #2", "My Event #3", "My Event #4", "My Event #1", "My Event #2", "My Event #3", "My Event #4", "My Event #1", "My Event #2", "My Event #3", "My Event #4", "My Event #1", "My Event #2", "My Event #3", "My Event #4", "My Event #1", "My Event #2", "My Event #3", "My Event #4", "My Event #1", "My Event #2", "My Event #3", "My Event #4", "My Event #1", "My Event #2", "My Event #3", "My Event #4"]
+            "events" : sorted(events)
         }
         return render(request, 'Calendar/calendar.html', context)
     else:
